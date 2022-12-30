221c529c4beacbcc6543334ba193a0a65ac64be3
Harshit Agrawal <financial.freeedom.venture@gmail.com>
2022-12-31 02:58:11 +0530

    updaets

---
+++ run_process.sh
@@ -0,0 +1,8 @@
+cd /home/raspberry/halfbottle-data
+
+# backend deployment - 5 instance
+nohup python3 -m uvicorn backend_process.main:app --host 0.0.0.0 --port 8001 &
+nohup python3 -m uvicorn backend_process.main:app --host 0.0.0.0 --port 8002 &
+nohup python3 -m uvicorn backend_process.main:app --host 0.0.0.0 --port 8003 &
+nohup python3 -m uvicorn backend_process.main:app --host 0.0.0.0 --port 8004 &
+nohup python3 -m uvicorn backend_process.main:app --host 0.0.0.0 --port 8005 &
--- run_server.sh
+++ run_server.sh
@@ -1,8 +1,4 @@
 cd /home/raspberry/halfbottle-data
 
 # backend deployment - 5 instance
-nohup python3 -m uvicorn backend.main:app --host 0.0.0.0 --port 8001 &
-nohup python3 -m uvicorn backend.main:app --host 0.0.0.0 --port 8002 &
-nohup python3 -m uvicorn backend.main:app --host 0.0.0.0 --port 8003 &
-nohup python3 -m uvicorn backend.main:app --host 0.0.0.0 --port 8004 &
-nohup python3 -m uvicorn backend.main:app --host 0.0.0.0 --port 8005 &
+nohup python3 -m uvicorn backend.main:app --host 0.0.0.0 --port 7000 &
