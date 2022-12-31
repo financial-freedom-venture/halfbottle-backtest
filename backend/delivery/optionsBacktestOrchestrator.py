b57a68dd5e3cdd80f7b74954078b0421a69ac68f
Harshit Agrawal <financial.freeedom.venture@gmail.com>
2022-12-31 20:52:52 +0530

    updates

--- backend/delivery/optionsBacktestOrchestrator.py
+++ backend/delivery/optionsBacktestOrchestrator.py
@@ -10,9 +10,9 @@ router = APIRouter()
 
 
 ########################################################
-#                      Endpoints                       #
+#                     Distributed                      #
 ########################################################
-@router.post("/distributed", status_code=200)
+@router.post("/distributed/", status_code=200)
 def getDistributedTest(
     request: Request,
     start_date_day: int,
@@ -46,11 +46,7 @@ def getDistributedTest(
 
     return data.dict()
 
-
-########################################################
-#                      Endpoints                       #
-########################################################
-@router.post("/distributed/detailed", status_code=200)
+@router.post("/distributed/detailed/", status_code=200)
 def getDistributedTest(
     request: Request,
     start_date_day: int,
@@ -75,7 +71,82 @@ def getDistributedTest(
             status_code=400, detail="invalid dates provided"
         )
 
-    data = crud.testStrategy(start_date, end_date, requestData, True)
+    data = crud.testStrategyDistributed(
+        start_date, end_date, requestData, True)
+
+    if data == None:
+        raise HTTPException(
+            status_code=403, detail="Data not available on this date or No Entry Available"
+        )
+
+    return data.dict()
+
+
+########################################################
+#                       Single                        #
+########################################################
+@router.post("/", status_code=200)
+def getDistributedTest(
+    request: Request,
+    start_date_day: int,
+    start_date_month: int,
+    start_date_year: int,
+    end_date_day: int,
+    end_date_month: int,
+    end_date_year: int,
+    requestData: StrategyDataType,
+    crud: OptionsBacktestOrchestratorCrud = Depends(
+        getOptionsBacktestOrchestratorCrud)
+):
+    try:
+        start_date = datetime(
+            start_date_year, start_date_month, start_date_day)
+        end_date = datetime(end_date_year, end_date_month, end_date_day)
+    except Exception as e:
+        logging.warning(
+            f'intradayBacktest -- getIntradayTest Daily --> Validation error in date')
+        logging.warning(e)
+        raise HTTPException(
+            status_code=400, detail="invalid dates provided"
+        )
+
+    data = crud.testStrategy(start_date, end_date, requestData)
+
+    if data == None:
+        raise HTTPException(
+            status_code=403, detail="Data not available on this date or No Entry Available"
+        )
+
+    return data.dict()
+
+
+@router.post("/detailed/", status_code=200)
+def getDistributedTest(
+    request: Request,
+    start_date_day: int,
+    start_date_month: int,
+    start_date_year: int,
+    end_date_day: int,
+    end_date_month: int,
+    end_date_year: int,
+    requestData: StrategyDataType,
+    crud: OptionsBacktestOrchestratorCrud = Depends(
+        getOptionsBacktestOrchestratorCrud)
+):
+    try:
+        start_date = datetime(
+            start_date_year, start_date_month, start_date_day)
+        end_date = datetime(end_date_year, end_date_month, end_date_day)
+    except Exception as e:
+        logging.warning(
+            f'intradayBacktest -- getIntradayTest Daily --> Validation error in date')
+        logging.warning(e)
+        raise HTTPException(
+            status_code=400, detail="invalid dates provided"
+        )
+
+    data = crud.testStrategy(
+        start_date, end_date, requestData, True)
 
     if data == None:
         raise HTTPException(
